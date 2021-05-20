550d3725e3a26c522d10aaa567cc806727d54500
JT <jfleetw@clemson.edu>
2021-05-19 21:54:28 -0400

    Following Changes Made:
    1. Made further progress implementing PongActivity class.
    	1a. Creating variables/invoking method to retrieve/store display
    	resolution information. (onCreate method)
    	1b. Calling PongGame constructor and using newly instantiated
    	object as paramater to set content view. (onCreate method)
    	1c. Begain implementing onResume and onPause methods.
    
    2. Made considerable progress in further implementing PongGame class.
    	2a. Subclassing under SurfaceView (is now of type view).
    	2b. Declaring instance variables and constants for use in future
    	implementation.
    	2c. Implemented parametrized constructor (takes in activity
    	class type, and resolution information)
    	2d. Assigning font size and font margin in relation to
    	resolution.
    	2e. Began implementation of startNew game method.
    	2f. Began implementation of draw method.
    	2g. Began implementation of debugging method. (FPS Information)
    
    3. Added new files relating to build (gradle).

--- app/src/main/java/com/gamecodeschool/pong/PongActivity.java
+++ app/src/main/java/com/gamecodeschool/pong/PongActivity.java
@@ -1,13 +1,40 @@
 package com.gamecodeschool.pong;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.view.Window;
 import android.os.Bundle;
+import android.graphics.Point;
+import android.view.Display;
 
-public class MainActivity extends AppCompatActivity {
+public class PongActivity extends Activity {
+    private PongGame mPongGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Display display = getWindowManager().getDefaultDisplay();
+
+        Point size = new Point();
+
+        display.getSize(size);
+
+        mPongGame = new PongGame(this, size.x, size.y);
+
+        setContentView(mPongGame);
+    }
+
+    @Override
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+
+    protected void onPause() {
+        super.onPause();
     }
 }
--- app/src/main/java/com/gamecodeschool/pong/PongGame.java
+++ app/src/main/java/com/gamecodeschool/pong/PongGame.java
@@ -1,4 +1,82 @@
 package com.gamecodeschool.pong;
 
-public class PongGame {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class PongGame extends SurfaceView {
+    private final boolean DEBUGGING = true;
+    private SurfaceHolder mOurHolder;
+    private Canvas mCanvas;
+    private Paint mPaint;
+    private long mFPS;
+    private final int MILLIS_IN_SECOND = 1000;
+    private int mScreenX;
+    private int mScreenY;
+    private int mFontSize;
+    private int mFontMargin;
+    private Bat mBat;
+    private Ball mBall;
+    private int mScore;
+    private int mLives;
+
+    public PongGame(Context context, int x, int y){
+        // of type view class that takes in activity class.
+        super(context);
+        mScreenX = x;
+        mScreenY = y;
+
+        mFontSize = mScreenY / 20;
+        mFontMargin = mScreenX / 75;
+
+        mOurHolder = getHolder();
+        mPaint = new Paint();
+
+        startNewGame();
+
+    }
+
+    public void startNewGame() {
+        mScore = 0;
+        mLives = 3;
+
+    }
+
+    private void draw() {
+        // Validates that area of memory that we want to manipulate to represent our frame of drawing is available.
+        // Drawing/processing takes place asynchronously with the code that detects player input and OS messages.
+        // Code executing 60 times a second, and we need to confirm that we have access to the memory before we access.
+        if (mOurHolder.getSurface().isValid()) {
+            // Lock the canvas (graphics memory) ready to draw.
+            // Ensures that while we are accessing the memory here, no other code can access it.
+            mCanvas = mOurHolder.lockCanvas();
+            mCanvas.drawColor(Color.argb(255, 26, 128, 182));
+
+            mPaint.setTextSize(mFontSize);
+
+            mCanvas.drawText("Score: " + mScore + "   Lives: " + mLives, mFontMargin, mFontSize, mPaint);
+
+            if (DEBUGGING){
+                printDebuggingText();
+            }
+
+
+            // Frees up the memory to be accessed again, and posts the new canvas.
+            // Happens every single frame of animation.
+            mOurHolder.unlockCanvasAndPost(mCanvas);
+
+        }
+    }
+
+    private void printDebuggingText() {
+        int debugSize = mFontSize / 2;
+        int debugStart = 150;
+
+        mPaint.setTextSize(debugSize);
+
+        mCanvas.drawText("FPS: " + mFPS, 10, debugStart + debugSize, mPaint);
+    }
 }
